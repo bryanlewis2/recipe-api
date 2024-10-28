34addd432fa887c79764c7793435820919a76dba
bryanlewis2 <bryanalbertlewis@gmail.com>
2024-10-28 19:34:32 +0530

    Dockerfile changes

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['http://recipe-api-v1-b9ab5586572a.herokuapp.com/', 'localhost', '127.0.0.1', '0.0.0.0']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
